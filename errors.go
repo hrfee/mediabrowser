562c2667e9d3f91ecf1c19ab067842fb093755a7
Harvey Tindall <hrfee@protonmail.ch>
2024-08-29 14:14:21 +0100

    keep error data as string, include status code in detailed error
    Instead of unmarshaling to DetailedError, just store the response string
    data in it. In case the format changes. Who cares about formatting
    really, this isn't meant to be visible.

--- errors.go
+++ errors.go
@@ -1,7 +1,6 @@
 package mediabrowser
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 )
@@ -9,13 +8,48 @@ import (
 // most 404 errors are from UserNotFound, so this generic error doesn't really need any detail.
 type ErrNotFound error
 
+// DetailedError is sometimes returned by Jellyfin.
+// These details are only populated when mb.Verbose is true.
+type DetailedError struct {
+	Verbose bool
+	Code    int
+	Data    string `json:"data"`
+	/*
+	   Code     int
+	   Type     string `json:"type"`
+	   Title    string `json:"title"`
+	   Detail   string `json:"detail"`
+	   Instance string `json:"instance"
+	*/
+}
+
+func (err DetailedError) Details() string {
+	if !err.Verbose {
+		return ""
+	}
+	msg := err.Data
+	/*if err.Type != "" {
+		msg += "Type: " + err.Type
+	}
+	if err.Title != "" {
+		msg += "\nTitle: " + err.Title
+	}
+	if err.Detail != "" {
+		msg += "\nDetail: " + err.Detail
+	}
+	if err.Instance != "" {
+		msg += "\nInstance: " + err.Instance
+	}*/
+	return msg + "\n"
+}
+
 type ErrUnauthorized struct {
 	DetailedError
 }
 
 func (err ErrUnauthorized) Error() string {
-	msg := "Unauthorized, check credentials."
-	if err.verbose {
+	msg := fmt.Sprintf("%d Unauthorized, check credentials.", err.Code)
+	if err.Verbose {
 		msg += "\n" + err.Details()
 	}
 	return msg
@@ -27,24 +61,23 @@ type ErrForbidden struct {
 
 func (err ErrForbidden) Error() string {
 	msg := "forbidden, the user may not have the correct permissions."
-	if err.verbose {
+	if err.Verbose {
 		msg += "\n" + err.Details()
 	}
 	return msg
 }
 
 var (
-	NotFound ErrNotFound = errors.New("Resource not found.")
+	NotFound ErrNotFound = errors.New("resource not found")
 )
 
 type ErrUnknown struct {
-	code int
 	DetailedError
 }
 
 func (err ErrUnknown) Error() string {
-	msg := fmt.Sprintf("failed (code %d)", err.code)
-	if err.verbose {
+	msg := fmt.Sprintf("failed (code %d)", err.Code)
+	if err.Verbose {
 		msg += "\n" + err.Details()
 	}
 	return msg
@@ -56,46 +89,37 @@ func (mb *MediaBrowser) genericErr(status int, data string) (err error) {
 		err = nil
 		return
 	case 401, 400:
-		err = ErrUnauthorized{}
+		err = ErrUnauthorized{
+			DetailedError: DetailedError{
+				Data:    data,
+				Verbose: mb.Verbose,
+				Code:    status,
+			},
+		}
 	case 404:
 		err = NotFound
 		return
 	case 403:
-		err = ErrForbidden{}
+		err = ErrForbidden{
+			DetailedError: DetailedError{
+				Data:    data,
+				Verbose: mb.Verbose,
+				Code:    status,
+			},
+		}
 	default:
-		err = ErrUnknown{code: status}
+		err = ErrUnknown{
+			DetailedError: DetailedError{
+				Data:    data,
+				Verbose: mb.Verbose,
+				Code:    status,
+			},
+		}
 	}
-	if mb.Verbose && data != "" {
+	/*if mb.Verbose && data != "" {
 		json.Unmarshal([]byte(data), &err)
-	}
+	}*/
 	return err
-}
-
-// DetailedError is sometimes returned by Jellyfin.
-// These details are only populated when mb.Verbose is true.
-type DetailedError struct {
-	verbose  bool
-	Type     string `json:"type"`
-	Title    string `json:"title"`
-	Detail   string `json:"detail"`
-	Instance string `json:"instance"`
-}
-
-func (err DetailedError) Details() string {
-	msg := ""
-	if err.Type != "" {
-		msg += "Type: " + err.Type
-	}
-	if err.Title != "" {
-		msg += "\nTitle: " + err.Title
-	}
-	if err.Detail != "" {
-		msg += "\nDetail: " + err.Detail
-	}
-	if err.Instance != "" {
-		msg += "\nInstance: " + err.Instance
-	}
-	return msg + "\n"
 }
 
 type ErrUserNotFound struct {
@@ -110,7 +134,7 @@ func (err ErrUserNotFound) Error() string {
 	} else {
 		msg += "User with ID \"" + err.id + "\" not found."
 	}
-	if err.verbose {
+	if err.Verbose {
 		msg += "\n" + err.Details()
 	}
 	return msg
@@ -122,7 +146,7 @@ type ErrNoPolicySupplied struct {
 
 func (err ErrNoPolicySupplied) Error() string {
 	msg := "No (valid) policy was given."
-	if err.verbose {
+	if err.Verbose {
 		msg += "\n" + err.Details()
 	}
 	return msg
